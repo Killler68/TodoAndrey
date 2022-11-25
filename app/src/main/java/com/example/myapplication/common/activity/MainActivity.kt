b14c54c72fd1a6e8b151e43e6292cca7924bb127
VIP <belevitin2010@mail.ru>
2022-11-25 17:28:51 +0300

    Fix layout notes add

--- app/src/main/java/com/example/myapplication/common/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/common/activity/MainActivity.kt
@@ -3,8 +3,8 @@ package com.example.myapplication.common.activity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.FragmentAuthorization
 import com.example.myapplication.R
+import com.example.myapplication.authorization.FragmentAuthorization
 
 
 private const val WAITING_TIME = 2000
