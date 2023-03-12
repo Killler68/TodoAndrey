305ef81fe27d38f1f3fe9cf3335d376157b30870
VIP <belevitin2010@mail.ru>
2023-03-12 23:01:03 +0300

    Change fragmenAuthorization

--- app/src/main/java/com/example/myapplication/authorization/FragmentAuthorization.kt
+++ app/src/main/java/com/example/myapplication/authorization/FragmentAuthorization.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.authorization.viewmodel.AuthorizationViewModel
+import com.example.myapplication.common.flow.launchWhenViewCreated
 import com.example.myapplication.common.fragment.getViewModelFactory
 import com.example.myapplication.common.navigation.NavCommand
 import com.example.myapplication.databinding.FragmentAuthorizationBinding
@@ -37,23 +37,15 @@ class FragmentAuthorization : Fragment() {
         setupListeners()
     }
 
-    private fun setupObservables() =
-        viewModel.navCommand.observe(viewLifecycleOwner, ::onDataLoaded)
-
-    private fun onDataLoaded(navCommand: NavCommand) =
-        findNavController().navigate(navCommand.action, navCommand.command)
-
-    private fun setupListeners() {
-        binding.signIn.setOnClickListener {
-            if (binding.enterLogin.text.toString() == ""
-                &&
-                binding.enterPassword.text.toString() == ""
-            ) {
-                viewModel.navigateToNotes()
-            } else {
-                Toast.makeText(activity, INPUT_ERROR, Toast.LENGTH_LONG).show()
-            }
+    private fun setupObservables() {
+        launchWhenViewCreated {
+            viewModel.navCommand.observe(::onDataLoaded)
         }
-
     }
+
+    private fun onDataLoaded(navCommand: NavCommand) {
+        findNavController().navigate(navCommand.action, navCommand.command)
+    }
+
+    private fun setupListeners() {}
 }
--- app/src/main/java/com/example/myapplication/authorization/viewmodel/AuthorizationViewModel.kt
+++ app/src/main/java/com/example/myapplication/authorization/viewmodel/AuthorizationViewModel.kt
@@ -1,18 +1,18 @@
 package com.example.myapplication.authorization.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.myapplication.common.flow.createSharedFlow
 import com.example.myapplication.common.navigation.NavCommand
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 
 class AuthorizationViewModel(
     private val navigatorToNotes: AuthorizationNotesNavigatorUseCase
 ) : ViewModel() {
 
-    private var _navCommand: MutableLiveData<NavCommand> = MutableLiveData()
-    val navCommand: LiveData<NavCommand> = _navCommand
+    private var _navCommand: MutableSharedFlow<NavCommand> = createSharedFlow()
+    val navCommand: SharedFlow<NavCommand> = _navCommand.asSharedFlow()
 
-    fun navigateToNotes() {
-        _navCommand.postValue(navigatorToNotes())
-    }
+    fun navigateToNotes() = _navCommand.tryEmit(navigatorToNotes())
 }
