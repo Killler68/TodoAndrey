023a6d72c127074dffaab4119107ddabe43dfeae
VIP <belevitin2010@mail.ru>
2023-03-20 22:04:01 +0300

    Create weather layout

---
+++ app/src/main/java/com/example/myapplication/weather/WeatherFragment.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication.weather
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentWeatherBinding
+
+
+class WeatherFragment : Fragment() {
+
+    private var _binding: FragmentWeatherBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentWeatherBinding.inflate(inflater, container, false)
+        .also { _binding = it }
+        .root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
